ea634d04c10f57ee8f4c631c31386f6506c42363
Atsushi Nakatsugawa <atsushi@moongift.jp>
2011-11-13 15:43:23 +0900

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Socialpost::Application.config.secret_token = 'fcf33a186c3f2ed9d097fc2436634e3f69bd482fdffa0425debaa26e3628ffc6b1730f2092a85c9c7df7fdb45e962b75f4a2b737d35e29f780565f78caef07bb'
